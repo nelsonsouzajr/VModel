3225e3161e8a4a703c9ce0dd95f6659d7b9db8f3
nelsonsouzajr <nelson.souzajunior@gmail.com>
2021-04-20 09:45:35 -0300

    Lifecycle part.1

--- app/src/main/java/br/com/souza/vmodel/MainActivity.kt
+++ app/src/main/java/br/com/souza/vmodel/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.souza.vmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -20,10 +21,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        logar(valor = "OnCreate")
+
         initDados()
         initContador()
         initClick()
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        logar(valor = "OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        logar(valor = "OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        logar(valor = "OnPause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        logar(valor = "OnStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        logar(valor = "OnDestroy")
+    }
+
+    private fun logar(tag: String = "Ciclo de Vida", valor: String){
+        Log.d(tag, valor)
     }
 
     private fun validaContador(){
